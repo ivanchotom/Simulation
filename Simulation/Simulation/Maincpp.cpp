5a8d28a1334c17e3e3a21b59b5526489aa7f2d3b
Ivan Vukov (s5071537) <s5071537@bournemouth.ac.uk>
2019-07-25 02:03:46 +0100

    Adding Game class

--- Simulation/Simulation/Maincpp.cpp
+++ Simulation/Simulation/Maincpp.cpp
@@ -1,59 +1,22 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2019)
-and may not be redistributed without written permission.*/
-//My friend helped me set up git 
-//Thank you friend
-//Using SDL and standard IO
-#include <SDL.h>
-#include <stdio.h>
-#include <iostream>
+#define SDL_MAIN_HANDLED
+#include "Game.h"
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+Game *game = nullptr;
 
 int main(int argc, char* args[])
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+	SDL_SetMainReady();
+	game = new Game();
 
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	while (game->running())
 	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
-
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 100, 100, 100));
-
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Wait two seconds
-			SDL_Delay(2000);
-
-		}
+		game->handleEvents();
+		game->update();
+		game->render();
 	}
 
-	//Destroy window
-	SDL_DestroyWindow(window);
+	game->clean();
 
-	//Quit SDL subsystems
-	SDL_Quit();
 
 	return 0;
 }
